cd52d5c8bd109d8f514c1da8ea55cf555b25a606
LMMSimon <karrde54@hotmail.com>
2021-03-03 03:17:40 +0800

    Fixed Filetree

--- assets/js/waterfall.js
+++ assets/js/waterfall.js
@@ -102,6 +102,13 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 // Parallax work by Renan Breno codepen
 // ============================================================================================
